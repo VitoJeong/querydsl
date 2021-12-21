0076e5e1d236aa09f37650493aa7de982cd16a8d
vito <ckdghk987@gmail.com>
2021-12-21 21:41:10 +0900

    add: querydsl dependency

--- src/test/java/com/jpa/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/jpa/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,39 @@
 package com.jpa.querydsl;
 
+import com.jpa.querydsl.entity.Hello;
+import com.jpa.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h");
+        Hello findEntity = queryFactory
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(findEntity).isEqualTo(hello);
+        assertThat(findEntity.getId()).isEqualTo(hello.getId());
     }
 
 }
