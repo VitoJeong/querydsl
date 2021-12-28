6354c40f7d7132890fa7f7d3d568f8ce7659163e
vito <ckdghk987@gmail.com>
2021-12-28 22:53:07 +0900

    add: sort, paging test

--- src/test/java/com/jpa/querydsl/QueryDslBasicTest.java
+++ src/test/java/com/jpa/querydsl/QueryDslBasicTest.java
@@ -3,6 +3,7 @@ package com.jpa.querydsl;
 import com.jpa.querydsl.entity.Member;
 import com.jpa.querydsl.entity.QMember;
 import com.jpa.querydsl.entity.Team;
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
@@ -15,6 +16,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
+import static com.jpa.querydsl.entity.QMember.member;
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
@@ -83,7 +85,9 @@ public class QueryDslBasicTest {
     void startQuerydsl() {
         String mem1 = "member1";
 
+        // 인스턴스 생성방법 두가지
         QMember m = new QMember("m");
+        QMember member = QMember.member;
 
         Member findMember = queryFactory
                 .selectFrom(m)
@@ -93,4 +97,118 @@ public class QueryDslBasicTest {
 
         assertThat(findMember.getUsername()).isEqualTo(mem1);
     }
+
+    @Test
+    void search() {
+        String mem1 = "member1";
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq(mem1)
+                        .and(member.age.eq(10)))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo(mem1);
+
+    }
+
+    @Test
+    void searchAndParam() {
+        String mem1 = "member1";
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        // and로 조건 설정
+                        member.username.eq(mem1), 
+                        (member.age.eq(10))
+                )
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo(mem1);
+
+    }
+
+    @Test
+    void resultFetch() {
+        //List
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+
+        //단 건
+        Member findMember1 = queryFactory
+                .selectFrom(member)
+                .fetchOne();
+
+        //처음 한 건 조회
+        Member findMember2 = queryFactory
+                .selectFrom(member)
+                .fetchFirst();
+
+        //페이징에서 사용
+        // 쿼리를 두번 실행(카운트, 컨텐츠)
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        //count 쿼리로 변경
+        long count = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+    }
+
+    @Test
+    void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(),
+                        member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+    }
+
+    @Test
+    void paging() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                // 1페이지 스킵
+                .offset(1)
+                .limit(2)
+                .fetch();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test
+    void pagingWithCount() {
+        QueryResults<Member> result = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                // 1페이지 스킵
+                .offset(1)
+                // 2개씩 페이징
+                .limit(2)
+                .fetchResults();
+
+        assertThat(result.getTotal()).isEqualTo(4);
+        assertThat(result.getLimit()).isEqualTo(2);
+        assertThat(result.getOffset()).isEqualTo(1);
+        assertThat(result.getResults().size()).isEqualTo(2);
+        
+    }
+
 }
